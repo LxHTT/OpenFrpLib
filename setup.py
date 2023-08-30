247eaf0b51fc47ada3f5caa28ce502c474c0d3b2
LxHTT <lxhtz.dl@qq.com>
2023-08-30 15:20:39 +0800

    版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="OpenFrpLib",
-    version="1.0.4",
+    version="1.0.5",
     author="LxHTT",
     author_email="lxhtt@vip.qq.com",
     long_description=long_description,
