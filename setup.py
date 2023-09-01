e4920f8a68f8d6e57e849a8f7a037da3d810c9c7
LxHTT <lxhtz.dl@qq.com>
2023-09-01 22:25:31 +0800

    A | getUserProxies的flag和msg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name="OpenFrpLib",
-    version="1.1.0",
+    version="1.1.1",
     author="LxHTT",
     author_email="lxhtt@vip.qq.com",
     long_description=long_description,
