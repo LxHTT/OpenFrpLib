6ef4905d85a74cb030a60c153b5eee26719a2fc0
LxHTT <lxhtz.dl@qq.com>
2023-10-03 15:43:40 +0800

    O | 优化绕过代理

--- OpenFrpLib/NetworkController.py
+++ OpenFrpLib/NetworkController.py
@@ -1,13 +1,11 @@
 from requests import Session
 
-from requests import Session
-
 class Session(Session):
-    def __init__(self):
+    def __init__(self, BYPASS_SYSTEM_PROXY=False):
         super().__init__()
         #: Trust environment settings for proxy configuration, default
         #: authentication and similar.
-        self.trust_env = False
+        self.trust_env = BYPASS_SYSTEM_PROXY
 
 post = Session().post
 get = Session().get
@@ -17,7 +15,6 @@ def BYPASS_SYSTEM_PROXY(STATUS):
     '''
     Bypass the system proxy to allow requests to POST OpenFrp OPENAPI normally.
     '''
-    if STATUS:
-        Session.trust_env = False
-    elif not STATUS:
-        Session.trust_env = True
+    global post, get
+    post = Session(not STATUS).post
+    get = Session(not STATUS).get
